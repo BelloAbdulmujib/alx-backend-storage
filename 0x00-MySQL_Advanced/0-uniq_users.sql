6719d6287ad7868a1ea1452ecb3ba5365bf8a318
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-11 14:43:29 -1100

    0x00-MySQL_Advanced

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- Creates a table of users with some requirements.
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULLAUTO_INCREMENT PRIMARY KEY,
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
