1e29f31dec6f78d122bef33973702cb7b7b594eb
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-16 13:02:35 -1100

    102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -2,53 +2,41 @@
 '''Task 15's module.
 '''
 from pymongo import MongoClient
+from bson.son import SON
 
+def top_students(mongo_collection):
+    # MongoDB aggregation pipeline to calculate the average score and sort by it
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {"$avg": "$scores"}
+            }
+        },
+        {
+            "$sort": SON([("averageScore", -1)])
+        }
+    ]
+    
+    # this will run the aggregation pipeline
+    results = list(mongo_collection.aggregate(pipeline))
+    
+    return results
 
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
-
-
-def print_top_ips(server_collection):
-    '''Prints statistics about the top 10 HTTP IPs in a collection.
-    '''
-    print('IPs:')
-    request_logs = server_collection.aggregate(
-        [
-            {
-                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
-            },
-            {
-                '$sort': {'totalRequests': -1}
-            },
-            {
-                '$limit': 10
-            },
-        ]
-    )
-    for request_log in request_logs:
-        ip = request_log['_id']
-        ip_requests_count = request_log['totalRequests']
-        print('\t{}: {}'.format(ip, ip_requests_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
-    print_top_ips(client.logs.nginx)
-
-
-if __name__ == '__main__':
-    run()
+# usage
+if __name__ == "__main__":
+    # MongoDB connection string and database/collection
+    uri = "mongodb://belloabdulmujib:my_password@localhost:27017"
+    client = MongoClient(uri)
+    db = client['belloabdulmujib']
+    collection = db['js_students']
+    
+    # this will get top students
+    top_students_list = top_students(collection)
+    
+    # Prints the top students
+    for student in top_students_list:
+        print(student)
+    
+    # Close the connection
+    client.close()
