db86208e16a920f15ec9d5dc792ac0d73eea84e0
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-17 08:31:30 -1100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Class that stores an instance of the Redis client as a private variable
+'''Class that store an instance of the Redis client as a private variable
 '''
 
 import redis
@@ -9,10 +9,10 @@ from functools import wraps
 
 
 class Cache:
-    '''Represents an object for storing data in a Redis data storage.
+    '''Reps an object for storing data in a Redis data storage.
     '''
     def __init__(self) -> None:
-        '''Initializes a Cache instance.
+        '''This initializes a Cache instance.
         '''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
@@ -20,7 +20,7 @@ class Cache:
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''Stores a value in a Redis data storage and returns the key.
+        '''This stores a value in a Redis data storage and returns the key.
         '''
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
