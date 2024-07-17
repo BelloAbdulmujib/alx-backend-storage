3a07f732bd5751fb05fa9891bd94b445ef06f7cb
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-17 08:55:34 -1100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -65,16 +65,22 @@ def replay(fn: Callable) -> None:
 
 
 class Cache:
-    '''Represents an object for storing data in a Redis data storage.'''
-    def __init__(self):
-        '''Initializes a Cache instance.'''
+    '''Represents an object for storing data in a Redis data storage.
+    '''
+    def __init__(self) -> None:
+        '''Initializes a Cache instance.
+        '''
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
 
     def get(
             self,
--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+'''module to obtain the HTML content of a particular URL and returns it
+'''
+
+import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def get(self, key: str) -> str:
+        return self._redis.get(key)
+
+    def set(self, key: str, value: str, ex: int):
+        self._redis.set(key, value, ex=ex)
+
+    def incr(self, key: str):
+        self._redis.incr(key)
+
+cache = Cache()
+
+def count_requests(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        cache.incr(f"count:{url}")
+        cached_page = cache.get(f"cache:{url}")
+        if cached_page:
+            return cached_page.decode('utf-8')
+
+        page_content = func(url)
+        cache.set(f"cache:{url}", page_content, ex=10)
+        return page_content
+    return wrapper
+
+@count_requests
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response'''
+    response = requests.get(url)
+    return response.text
