4c3e94b2f6a2f246ed041af35414f677177258d1
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-17 08:22:54 -1100

    exercise.py

--- 0x02-redis_basics/exercise.py
+++ 0x02-redis_basics/exercise.py
@@ -4,15 +4,24 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Any, Callable
+from functools import wraps
 
 
 class Cache:
-    def __init__(self):
+    '''Represents an object for storing data in a Redis data storage.
+    '''
+    def __init__(self) -> None:
+        '''Initializes a Cache instance.
+        '''
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
